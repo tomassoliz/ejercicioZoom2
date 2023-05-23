7b2544380de1c696d48c4dc6ffd0bb36444f3df0
Tomas Soliz <tomassoliz07@gmail.com>
2023-05-23 20:26:58 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculadora = require("./calculadora");
+let resultado = 0;
+switch (process.argv[2]) {
+  case sumar:
+    resultado = calculadora.sumar(process.argv[3], process.argv[4]);
+    break;
+  case restar:
+    resultado = calculadora.restar(process.argv[3], process.argv[4]);
+    break;
+  case multiplicar:
+    resultado = calculadora.multiplicar(process.argv[3], process.argv[4]);
+    break;
+  case dividir:
+    resultado = calculadora.dividir(process.argv[3], process.argv[4]);
+    break;
+  default:
+    resultado = "ERROR: comando incorrecto";
+    break;
+}
---
+++ calculadora/index.js
@@ -0,0 +1,12 @@
+console.log(resultado);
+const sumar = require('./sumar');
+const restar = require('./restar');
+const multiplicar = require('./multiplicar');
+const dividir = require('./dividir');
+
+module.exports = {
+    sumar,
+    restar,
+    multiplicar,
+    dividir
+}
